7777c1c958c7fea3829bb14b777c48302577d1e6
Ali Khazendar <cph-ak268@cphbusiness.dk>
2017-05-16 14:09:29 +0200

    1

---
+++ JFog/src/main/java/Service/Entity/Orders.java
@@ -0,0 +1,68 @@
+package Service.Entity;
+
+import java.sql.Date;
+
+public class Orders {
+   
+    private int ono;
+    private Date checkout;
+    private Date deliviry;
+    private double shipping;
+    private int carportmaterial_id;
+
+    public Orders(int ono, Date checkout, Date deliviry, double shipping, int carportmaterial_id) {
+        this.ono = ono;
+        this.checkout = checkout;
+        this.deliviry = deliviry;
+        this.shipping = shipping;
+        this.carportmaterial_id = carportmaterial_id;
+    }
+
+    public int getOno() {
+        return ono;
+    }
+
+    public void setOno(int ono) {
+        this.ono = ono;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(Date checkout) {
+        this.checkout = checkout;
+    }
+
+    public Date getDeliviry() {
+        return deliviry;
+    }
+
+    public void setDeliviry(Date deliviry) {
+        this.deliviry = deliviry;
+    }
+
+    public double getShipping() {
+        return shipping;
+    }
+
+    public void setShipping(double shipping) {
+        this.shipping = shipping;
+    }
+
+    public int getcarportmaterial_id() {
+        return carportmaterial_id;
+    }
+
+    public void setcarportmaterial_id(int carportmaterial_id) {
+        this.carportmaterial_id = carportmaterial_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Orders{" + "ono=" + ono + ", checkout=" + checkout + ", deliviry=" + deliviry + ", shipping=" + shipping + ", orderline_id=" + carportmaterial_id + '}';
+    }
+
+ 
+    
+}
---
+++ JFog/src/main/java/data/exception/CarportMaterialException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data.exception;
+
+/**
+ *
+ * @author KH
+ */
+public class CarportMaterialException extends Exception {
+
+    /**
+     * Creates a new instance of <code>CarportMaterialException</code> without
+     * detail message.
+     */
+    public CarportMaterialException() {
+    }
+
+    /**
+     * Constructs an instance of <code>CarportMaterialException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public CarportMaterialException(String msg) {
+        super(msg);
+    }
+}
